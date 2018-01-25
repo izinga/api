eb7e33b3aafd6a75be540664c332b7d7503563c9
Om Narayan <omnarayan@robustest.com>
2018-01-25 12:16:36 +0530

    removed all old apis

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 cp swagger.yaml ./docs/
-swagger-codegen generate -i swagger.yaml  -l python -o client/python
-swagger-codegen generate -i swagger.yaml  -l java -o client/java
-swagger-codegen generate -i swagger.yaml  -l ruby -o client/ruby
-swagger-codegen generate -i swagger.yaml  -l go -o client/go
-swagger-codegen generate -i swagger.yaml  -l php -o client/php
-swagger-codegen generate -i swagger.yaml  -l perl -o client/perl
-swagger-codegen generate -i swagger.yaml  -l javascript -o client/javascript
-swagger-codegen generate -i swagger.yaml  -l csharp -o client/csharp
-swagger-codegen generate -i swagger.yaml  -l swift -o client/swift
+# swagger-codegen generate -i swagger.yaml  -l python -o client/python
+# swagger-codegen generate -i swagger.yaml  -l java -o client/java
+# swagger-codegen generate -i swagger.yaml  -l ruby -o client/ruby
+# swagger-codegen generate -i swagger.yaml  -l go -o client/go
+# swagger-codegen generate -i swagger.yaml  -l php -o client/php
+# swagger-codegen generate -i swagger.yaml  -l perl -o client/perl
+# swagger-codegen generate -i swagger.yaml  -l javascript -o client/javascript
+# swagger-codegen generate -i swagger.yaml  -l csharp -o client/csharp
+# swagger-codegen generate -i swagger.yaml  -l swift -o client/swift
 
